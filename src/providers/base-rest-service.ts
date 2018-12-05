16842472b232e0dd077dff1f3a0f6040a7426bc6
Leonardo Correia <leonardo.correia.96@outlook.com>
2018-12-05 06:28:39 -0200

    userId

--- src/providers/base-rest-service.ts
+++ src/providers/base-rest-service.ts
@@ -46,19 +46,9 @@ export class BaseRestService {
         headers.append('Content-Type', 'application/json');
 
         var requestFunctions = {
-            get: function (data: any): Observable<T> {
-
-                let customUrl;
-
-                if (data)
-                {
-                    customUrl = url + "/" + data.id;
-                }else
-                {
-                    customUrl = url;
-                }
-                
-                return self.http.get(customUrl, { headers: headers })
+            get: function (data: any, customUrl): Observable<T> {
+                url += customUrl;
+                return self.http.get(url, { headers: headers })
                     .map((data: Response) => data.json())
                     .catch((err: Response) => {
                         return Observable.throw(err);
